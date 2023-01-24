479a2820659cbc97855106b89e0201fbbb61dac7
ncocana <ncocana@cifpfbmoll.eu>
2023-01-24 21:06:13 +0100

    feat(dml): add example data to tables

--- scripts/dml.sql
+++ scripts/dml.sql
@@ -2,4 +2,4 @@
 INSERT INTO Bikes( type, mark, isElectric, capacity, price) values ('urban bike', 'BTWIN', 0, 1, 20);
 
 -- Insert some data in the table Store
-INSERT INTO Store(StoreID, name, phone_number) values (1475, 'Balance Toys', 971548680);
+INSERT INTO Store(StoreID, name, address, email, phone_number) values (1475, 'Balance Toys', 'direccion A', 'btoys@gmail.com', 971548680);
---
+++ scripts/queries.sql
@@ -0,0 +1,3 @@
+-- Gets the counts of Bikes that are electric.
+select count(*) from Bikes where isElectric = '';
+
