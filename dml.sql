bd641eda4e80e53fce91444c6d9a34436cd88e59
SPiedra955 <spiedramite@cifpfbmoll.eu>
2023-01-24 21:02:31 +0100

    inserts's

---
+++ dml.sql
@@ -0,0 +1,5 @@
+-- Insert some data in the table Bikes
+INSERT INTO Bikes( type, mark, isElectric, capacity, price) values ('urban bike', 'BTWIN', 0, 1, 20);
+
+-- Insert some data in the table Store
+INSERT INTO Store(StoreID, name, phone_number) values (1475, 'Balance Toys', 971548680);
