46d9a3b0e2c0aeffae00476214071106cfa680a3
SPiedra955 <spiedramite@cifpfbmoll.eu>
2023-01-25 20:21:36 +0100

    script for delete updated

--- scripts/delete.sql
+++ scripts/delete.sql
@@ -1,3 +1,13 @@
 -- This is a query for delete all the data store in the tables.
 -- Also we can add the keyword WHERE for delete a specific data.
+
+-- Erase all the data stored in 'Bikes'
 DELETE FROM Bikes;
+
+-- Erase all the data stored in 'Store'
+DELETE FROM Store;
+
+-- Erase all the data stored in 'Stock'
+DELETE FROM Stock;
+
+-- When this queries are executed we should have all the tables empty.
