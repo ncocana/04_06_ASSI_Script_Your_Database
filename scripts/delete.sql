85cd81013039747b78c6e77bcbd0c77a5a509eab
ncocana <ncocana@cifpfbmoll.eu>
2023-01-30 16:47:17 +0100

    fix(delete.sql): allow to delete all the tables at once

--- scripts/delete.sql
+++ scripts/delete.sql
@@ -1,6 +1,10 @@
 -- This is a query for delete all the data store in the tables.
 -- Also we can add the keyword WHERE for delete a specific data.
 
+-- Deactivates the foreign keys with the purpose of
+-- avoiding errors when deleting the data from the tables.
+set foreign_key_checks = 0;
+
 -- Erase all the data stored in 'Bikes'
 DELETE FROM Bikes;
 
@@ -9,5 +13,8 @@ DELETE FROM Store;
 
 -- Erase all the data stored in 'Stock'
 DELETE FROM Stock;
+
+-- Activates the foreign keys again.
+set foreign_key_checks = 1;
 
 -- When this queries are executed we should have all the tables empty.
